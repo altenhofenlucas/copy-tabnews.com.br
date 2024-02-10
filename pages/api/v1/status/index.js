b19e39bac309f1a86563c428ca7988153aea2ad0
Lucas Altenhofen <lucas.a.bertrand@gmail.com>
2024-02-10 13:50:26 +0000

    ci: add  and  files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result.rows);
   return response.status(200).json({
     message: "test",
   });
